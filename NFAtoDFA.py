8195c09b2d3b4e87bff547a694e156d6c2708ea2
rtperezyera <tllezroberto9@gmail.com>
2019-06-19 15:13:31 -0500

    Create NFAtoDFA.py

---
+++ NFAtoDFA.py
@@ -0,0 +1,9 @@
+# Encoding: UTF-8
+# Authors: Javier Martínez Hernández, Roberto Téllez Perezyera
+"""
+This program is rlly cool thks for using it
+"""
+import sys
+
+it = iter(sys.stdin.read().splitlines())
+#n = int(next(it))
