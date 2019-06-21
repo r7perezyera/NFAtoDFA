8c371a37415e3c0b2cc2c771ff555bff6f2525bd
rtperezyera <tllezroberto9@gmail.com>
2019-06-21 10:49:29 -0500

    Update NFAtoDFA.py
    Add read input and get alphabets.

--- NFAtoDFA.py
+++ NFAtoDFA.py
@@ -4,6 +4,31 @@
 This program is rlly cool thks for using it
 """
 import sys
-
 it = iter(sys.stdin.read().splitlines())
-#n = int(next(it))
+
+# read input and create a list of triplets
+triplets = next(it).split("),(")
+triplets[0] = triplets[0][2::]
+triplets[-1] = triplets[-1][:-2:]
+print(triplets)
+
+
+
+sigmaN = []
+
+for triplet in triplets:
+    if triplet[0] not in sigmaN:
+        sigmaN.append(triplet[0])
+
+# should we int them tho?
+# this block does not work
+for symbol in sigmaN:
+    int(symbol)
+
+sigmaD = sigmaN
+
+print("SigmaN:",sigmaN)
+print("Thus, sigmaD:",sigmaD)
+
+
+
